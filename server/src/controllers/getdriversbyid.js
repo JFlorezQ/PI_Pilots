e7de3eed1bf04342faaeaf89514033d832927fca
Juliana <jflorez@proton.me>
2023-10-11 21:33:45 -0500

    Ruta GetDriversbyid terminada

--- server/src/controllers/getdriversbyid.js
+++ server/src/controllers/getdriversbyid.js
@@ -17,36 +17,73 @@ Escuderías.*/
 
 const axios = require('axios');
 const URL = `http://localhost:5000/drivers`;
+const{Driver} = require('../db.js')
 
 const getDriverById = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const { data } = await axios.get(`${URL}/${id}`);
+  try {
+    const { id } = req.params;
 
-        if (!data.name) {
-            throw new Error(`Faltan datos del conductor con ID: ${id}`);
-        }
-        
-        const driver = {
-            id: data.id,
-            name: data.name.forename,
-            surname: data.name.surname,
-            nationality: data.nationality,
-            description: data.description,
-            image: data.image,
-            dob: data.dob,
-            teams: data.teams
-        };
+    const uuidRegExp = /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/;
 
-        return res.status(200).json(driver);
-    } catch (error) {
-        if (error.response) {
-            return res.status(500).send(error.response.data.error);
-        } else {
-            return res.status(404).send(error.message);
-        }
+    let driver;
+    
+    //evalua si el id es un integro
+
+    if (/^\d+$/.test(id)) {
+      // Si el ID es un número, busca el conductor en la API
+      const response = await axios.get(`${URL}/${id}`);
+
+      if (!response.data.name) {
+        throw new Error(`Faltan datos del conductor con ID: ${id}`);
+      }
+      // Si encuentra el id, la constante driver toma estos datos
+      driver = {
+        id: response.data.id,
+        name: response.data.name.forename,
+        surname: response.data.name.surname,
+        nationality: response.data.nationality,
+        description: response.data.description,
+        image: response.data.image,
+        dob: response.data.dob,
+        teams: response.data.teams,
+      };
+    } else if (uuidRegExp.test(id)) {
+      // Si el ID es un UUID, busca el conductor en la base de datos
+      const driverFromDB = await Driver.findByPk(id);
+
+      //Si no lo encuentra en la base de datos tira el error
+
+      if (!driverFromDB) {
+        throw new Error(`Faltan datos del conductor con ID: ${id}`);
+      }
+
+      //Si encuentra el id driver toma estos datos
+
+
+      driver = {
+        id: driverFromDB.id,
+        name: driverFromDB.name,
+        surname: driverFromDB.surname,
+        nationality: driverFromDB.nationality,
+        description: driverFromDB.description,
+        image: driverFromDB.image,
+        dob: driverFromDB.dob,
+        teams: driverFromDB.teams,
+      };
     }
+
+    else { throw new Error(`${id} es un tipo de dato incorrecto`)}
+    // de cualquier modo si no hay error retorna driver
+ 
+    return res.status(200).json(driver);
+ 
+} catch (error) {
+    if (error.response) {
+      return res.status(500).send(error.response.data.error);
+    } else {
+      return res.status(404).send(error.message);
+    }
+  }
 };
 
 module.exports = { getDriverById };
-
