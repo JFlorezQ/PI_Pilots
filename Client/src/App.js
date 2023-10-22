c487515bbd26164ac3420caa845b9e5b401b084c
Juliana <jflorez@proton.me>
2023-10-21 22:05:28 -0500

    ya tengo el detail y las routes

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,19 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Create from './Views/Create/Create'
 import Detail from './Views/Detail/Detail'
 import Home from './Views/Home/Home'
 import Landing from './Views/Landing/Landing'
 
-import {Routes, Route, useLocation, useNavigate} from 'react-router-dom'
+import {Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
     <div>
-      <Create/>
-      <Detail/>
-      <Home/>
-      <Landing/>
+      <Routes>
+        <Route path="/" element = {<Landing/>}/>
+        <Route path="/home" element = {<Home/>}/>
+        <Route path="/detail/:id" element = {<Detail/>}/>
+        <Route path="/Create" element = {<Create/>}/>
+      </Routes>
+      
     </div>
     )
 }
--- Client/src/Views/Detail/Detail.jsx
+++ Client/src/Views/Detail/Detail.jsx
@@ -1,9 +1,33 @@
 import './Detail.css';
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from 'react';
 
 function Detail() {
-  return (
-    <h1> Este es mi detail </h1>
-  )
-}
+    const { id } = useParams();
+    const [driver, setDriver] = useState({});    
 
+    useEffect(() => {
+        fetch(`http://localhost:1030/drivers/${id}`)
+            .then(response => response.json())
+            .then(data => {
+                if (data.name) {
+                    setDriver(data);
+                } else {
+                    window.alert("No hay Driver con este ID");
+                }
+            })
+    }, [id]);
+
+    return (
+        <div className="container">
+            <img src={driver?.image?.url} alt={driver?.name} />
+            <h1>Name: {driver?.name} {driver?.surname}</h1>
+            <h2>ID: {driver?.id}</h2>
+            <h2>Nationality: {driver?.nationality}</h2>
+            <h3>Description: {driver?.description}</h3>
+            <h2>Date of birth: {driver?.dob}</h2>
+            <h2>Teams: {driver?.teams}</h2>
+        </div>
+    );
+}
 export default Detail;
